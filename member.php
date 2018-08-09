1bbb721aa8236c5742834016966adf61f3ba9b8c
Jerry Wong <jerry.wong@iproperty.com>
2018-08-09 21:14:46 +0800

    add fb

---
+++ member.php
@@ -0,0 +1,15 @@
+<?php
+  $fb = new Facebook\Facebook([
+    'app_id' => '1558355577612796', // 把 {app_id} 換成你的應用程式編號
+    'app_secret' => '3b791c12364219428b76909e95391f5d', // 把 {app_secret} 換成你的應用程式密鑰
+    'default_graph_version' => 'v2.2',
+    ]);
+  
+  $helper = $fb->getRedirectLoginHelper();
+  
+  $permissions = ['email'];
+  $loginUrl = $helper->getLoginUrl('https://traffiter.com/member.php', $permissions);
+  
+  echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+
+  ?>
